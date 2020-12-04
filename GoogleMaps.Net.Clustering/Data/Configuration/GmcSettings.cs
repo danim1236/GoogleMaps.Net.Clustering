81cffdacfbf094e9928d03d5450eb71b1c8044cf
Daniel Mendes <daniel@danim.com.br>
2020-12-03 21:41:18 -0300

    permitindo setar o GmcSettings

--- GoogleMaps.Net.Clustering/Data/Configuration/GmcSettings.cs
+++ GoogleMaps.Net.Clustering/Data/Configuration/GmcSettings.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
-using GoogleMaps.Net.Clustering.Contract;
 
 namespace GoogleMaps.Net.Clustering.Data.Configuration
 {
@@ -20,12 +19,14 @@ namespace GoogleMaps.Net.Clustering.Data.Configuration
         /// <summary>
         /// Singleton
         /// </summary>
-        public static IGmcSettings Get
+        public static IGmcSettings Get => _algoConfig ?? (_algoConfig = new GmcSettings());
+
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static IGmcSettings Set
         {
-            get
-            {
-                return _algoConfig ?? (_algoConfig = new GmcSettings());
-            }
+            set => _algoConfig = value;
         }
 
         private GmcSettings()
@@ -53,50 +54,50 @@ namespace GoogleMaps.Net.Clustering.Data.Configuration
         }
 
         // Use debug data
-        public bool DoShowGridLinesInGoogleMap { get; private set; } // generate draw grid lines info to google map
+        public bool DoShowGridLinesInGoogleMap { get; set; } // generate draw grid lines info to google map
 
         // How much data that is send to client
         // EDIT to extend to widen or shorten gridview for outside view, must be minimum 0
         // default value is 1 which returns same data as illustrated in the picture from my blog
         // (see googlemaps-clustering-viewport_ver1.png inside the Docements/Design folder)
-        public int OuterGridExtend { get; private set; }
+        public int OuterGridExtend { get; set; }
 
         // Move centroid point to nearest existing point?
-        public bool DoUpdateAllCentroidsToNearestContainingPoint { get; private set; } 
+        public bool DoUpdateAllCentroidsToNearestContainingPoint { get; set; } 
         
         // Merge clusterpoints if close to each other?
-        public bool DoMergeGridIfCentroidsAreCloseToEachOther { get; private set; } 
+        public bool DoMergeGridIfCentroidsAreCloseToEachOther { get; set; } 
         
         // Cache get markers and get markers info services
-        public bool CacheServices { get; private set; }
+        public bool CacheServices { get; set; }
 
         // If neighbor cluster is within 1/n dist then merge, heuristic, higher value gives less merging
-        public double MergeWithin { get; private set; }
+        public double MergeWithin { get; set; }
 
         // Only cluster if minimum this number of points
-        public int MinClusterSize { get; private set; }
+        public int MinClusterSize { get; set; }
 
         // If clustering is disabled, restrict number of markers returned
-        public int MaxMarkersReturned { get; private set; }
+        public int MaxMarkersReturned { get; set; }
 
         // Always cluster if equal or below this zoom level
         // to disable this effect set the value to -1
-        public int AlwaysClusteringEnabledWhenZoomLevelLess { get; private set; }
+        public int AlwaysClusteringEnabledWhenZoomLevelLess { get; set; }
 
         // Stop clustering from this zoom level and larger
-        public int ZoomlevelClusterStop { get; private set; }
+        public int ZoomlevelClusterStop { get; set; }
 
         // Grid array
-        public int GridX { get; private set; }
-        public int GridY { get; private set; }
+        public int GridX { get; set; }
+        public int GridY { get; set; }
 
         // Array of existing marker types
-        public HashSet<int> MarkerTypes { get; private set; }
+        public HashSet<int> MarkerTypes { get; set; }
 
         // Max allowed points in memory cache
-        public int MaxPointsInCache { get; private set; }
+        public int MaxPointsInCache { get; set; }
     
-        public string Environment { get; private set; }
+        public string Environment { get; set; }
 
         public NameValueCollection GetGoogleMapsSection()
         {
@@ -105,7 +106,7 @@ namespace GoogleMaps.Net.Clustering.Data.Configuration
 
         static string Throw(string s)
         {
-            throw new Exception(string.Format("GmcGlobalKeySettings setup error: {0}", s));
+            throw new Exception($"GmcGlobalKeySettings setup error: {s}");
         }       
     }
 }
